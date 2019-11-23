0a59ddc0baac2f8074a2fe03d0567cf27848a6a2
watanabe@tiffa <watanabe.everystudio@gmail.com>
2019-11-23 21:02:26 +0900

    スクリプト追加

---
+++ TouchPadTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchPadTest : MonoBehaviour {
+
+	public Vector2 touch;
+
+	public void start()
+	{
+		Debug.Log("start");
+	}
+
+	public void touchpad(Vector2 _touch)
+	{
+		touch += _touch;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
